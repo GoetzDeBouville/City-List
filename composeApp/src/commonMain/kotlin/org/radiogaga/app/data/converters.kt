b833171ec56edab40ea6c3521b8ce376af04513a
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-15 15:49:23 +0300

    add string resources fix colors and theme

--- composeApp/src/commonMain/kotlin/org/radiogaga/app/data/converters.kt
+++ composeApp/src/commonMain/kotlin/org/radiogaga/app/data/converters.kt
@@ -1,10 +1,16 @@
 package org.radiogaga.app.data
 
+import city_list.composeapp.generated.resources.Res
+import city_list.composeapp.generated.resources.country
+import city_list.composeapp.generated.resources.population
 import org.radiogaga.app.data.dto.CityDto
 import org.radiogaga.app.domain.City
 
 fun CityDto.toDomain(): City {
-    val subtTitle = "Страна: ${this.country} население: ${this.population}"
+    val subtTitle = Res.string.country.toString() +
+            this.country +
+            Res.string.population.toString() +
+            this.population
 
     return City(
         title,
