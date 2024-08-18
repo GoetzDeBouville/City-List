2be11228de033f49d80756d91a0d9e038a068ad2
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-18 18:52:22 +0300

    add store factory

--- composeApp/src/commonMain/kotlin/org/radiogaga/app/feature/search/store/SearchStore.kt
+++ composeApp/src/commonMain/kotlin/org/radiogaga/app/feature/search/store/SearchStore.kt
@@ -1,9 +1,19 @@
 package org.radiogaga.app.feature.search.store
 
+import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import org.radiogaga.app.core.domain.model.City
 import org.radiogaga.app.core.domain.model.ErrorType
+import org.radiogaga.app.core.domain.model.Result
 import org.radiogaga.app.core.ui.ErrorScreenState
+import org.radiogaga.app.feature.search.domain.usecase.GetCitiesUseCase
 import org.radiogaga.app.feature.search.store.SearchStore.Intent
 import org.radiogaga.app.feature.search.store.SearchStore.Label
 
@@ -33,4 +43,110 @@ internal interface SearchStore : Store<Intent, SearchStore.State, Label> {
     }
 
     sealed interface Label
+}
+
+internal class SearchStoreFactory(
+    private val storeFactory: StoreFactory,
+    private val getCitiesUseCase: GetCitiesUseCase
+) {
+
+    fun create(): SearchStore =
+        object : SearchStore, Store<Intent, SearchStore.State, Label> by storeFactory.create(
+            name = "SearchStore",
+            initialState = SearchStore.State(),
+            reducer = ReducerImpl,
+            executorFactory = { ExecutorImpl(getCitiesUseCase) }
+        ) {}
+
+    private object ReducerImpl : Reducer<SearchStore.State, SearchStore.Msg> {
+        override fun SearchStore.State.reduce(msg: SearchStore.Msg): SearchStore.State {
+            return when (msg) {
+                is SearchStore.Msg.Empty -> copy(
+                    cityList = emptyList(),
+                    errorType = ErrorScreenState.NOTHING_FOUND,
+                    isLoading = false
+                )
+
+                is SearchStore.Msg.Loading -> copy(isLoading = true)
+                is SearchStore.Msg.LoadError -> copy(
+                    errorType = mapErrorToUiState(msg.error),
+                    isLoading = false
+                )
+
+                is SearchStore.Msg.CitiesLoaded -> copy(cityList = msg.cities, isLoading = false)
+            }
+        }
+
+        private fun mapErrorToUiState(errorType: ErrorType): ErrorScreenState {
+            return when (errorType) {
+                ErrorType.NO_CONNECTION -> ErrorScreenState.NO_INTERNET
+                ErrorType.NOTHING_FOUND -> ErrorScreenState.NOTHING_FOUND
+                else -> ErrorScreenState.SERVER_ERROR
+            }
+        }
+    }
+
+    private class ExecutorImpl(private val getCitiesUseCase: GetCitiesUseCase) :
+        CoroutineExecutor<Intent, SearchStore.Action, SearchStore.State, SearchStore.Msg, Nothing>() {
+
+        override fun executeIntent(intent: Intent) {
+            when (intent) {
+                is Intent.SearchTextChanged -> getCityListByQuery(intent)
+
+                is Intent.ClearSearch -> dispatch(SearchStore.Msg.Empty)
+            }
+        }
+
+        private var queryString = ""
+
+        private fun getCityListByQuery(
+            intent: Intent.SearchTextChanged
+        ) {
+            val formatedQuery = formatString(intent.query)
+            if (queryString == formatedQuery) {
+                println("Same string")
+            } else {
+                queryString = formatedQuery
+                dispatch(SearchStore.Msg.Loading(true))
+                runSafelyUseCase(
+                    useCaseFlow = getCitiesUseCase.execute(formatedQuery),
+                    scope,
+                    onSuccess = { cities ->
+                        dispatch(SearchStore.Msg.CitiesLoaded(cities))
+                    },
+                    onFailure = { error ->
+                        dispatch(SearchStore.Msg.LoadError(error))
+                    }
+                )
+            }
+        }
+
+        private inline fun <reified D> runSafelyUseCase(
+            useCaseFlow: Flow<Result<D, ErrorType>>,
+            coroutineScope: CoroutineScope,
+            noinline onFailure: ((ErrorType) -> Unit)? = null,
+            crossinline onSuccess: (D) -> Unit,
+        ) {
+            coroutineScope.launch(Dispatchers.IO) {
+                runCatching {
+                    useCaseFlow.collect { result ->
+                        when (result) {
+                            is Result.Success -> onSuccess(result.data)
+                            is Result.Error -> {
+                                onFailure?.invoke(result.error)
+                            }
+                        }
+                    }
+                }.onFailure { error ->
+                    error.printStackTrace()
+                    onFailure?.invoke(ErrorType.UNKNOWN_ERROR)
+                }
+            }
+        }
+
+        private fun formatString(str: String): String {
+            val noPunctuation = str.trim().replace(Regex("[^\\w\\s-]"), "")
+            return noPunctuation.replace(Regex("\\s+"), " ").trim()
+        }
+    }
 }
