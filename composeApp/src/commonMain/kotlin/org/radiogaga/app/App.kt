72331ba69e1095f5a4cb131ffd6659e47fc91799
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-09-20 18:52:24 +0300

    remove comments change string languages

--- composeApp/src/commonMain/kotlin/org/radiogaga/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/radiogaga/app/App.kt
@@ -1,6 +1,5 @@
 package org.radiogaga.app
 
-//import org.radiogaga.app.feature.search.ui.SearchScreenVM
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,17 +17,8 @@ internal fun App() = AppTheme {
             startDestination = Routes.SearchScreen.route
         ) {
             composable(route = Routes.SearchScreen.route) {
-//                val viewModel = koinViewModel<SearchScreenVM>()
                 SearchScreen(navController = rememberNavController())
             }
         }
     }
 }
-
-//@Composable
-//inline fun <reified T: ViewModel> koinViewModel() : T {
-//    val scope = currentKoinScope()
-//    return viewModel {
-//        scope.get<T>()
-//    }
-//}
